8f36243dbf3f29c67806dd3e16f9e46d716f65fc
Yauheni Kadyrau <7077634@gmail.com>
2024-04-29 17:55:57 +0300

    fix: change url to vercel

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "task4-itransition-mauve.vercel.app",
+  baseURL: "task4-itransition-65ds.vercel.app",
 });
 export const getUsers = async (token) => {
   const config = {
