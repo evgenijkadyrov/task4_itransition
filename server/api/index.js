8e976196aeca47af7240c383524eb1ab43162972
Yauheni Kadyrau <7077634@gmail.com>
2024-04-29 22:52:15 +0300

    fix: cors

--- server/api/index.js
+++ server/api/index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 const cors = require('cors');
 
-app.use(cors({}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
